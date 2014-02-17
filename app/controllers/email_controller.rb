7319d382e79c7dd534e22409fcb6d73d43942010
BBariStaR <warld2003@hanmail.net>
2014-02-18 01:41:13 +0900

    실제 이메일 전송 기능 탑재하기

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -3,8 +3,21 @@ def email_form
 end
 
 def email_send
-@email = params[:email]
-@content = params[:content]
+email = params[:email]
+content = params[:content]
+title = params[:title]
+
+gmail = Gmail.connect("Gmail_mail_address","Gmail_mail_password")
+gmail.deliver do
+  to email
+  subject title
+  text_part do
+    body content
+  end
+end
+
+gmail.logout
+
 end
 
 end
